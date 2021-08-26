1438a54ab63962841ce96166ca1683dbd19b5fd2
GitHub Action <action@github.com>
2021-08-26 06:14:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/reacoder/firstplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/reacoder/firstplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.reacoder.firstplugin.services
 
+import com.github.reacoder.firstplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
